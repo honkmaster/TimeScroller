1ea6137d7049a2c0b4ceb82fdc887df12f2862b5
Tobias Tiemerding <tobias.tiemerding@uni-oldenburg.de>
2014-05-03 11:37:44 +0200

    Update assets, mask layer, also dynamic resizing

--- TTTimeScroller/TTTimeScroller.m
+++ TTTimeScroller/TTTimeScroller.m
@@ -10,10 +10,9 @@
 
 @interface TTTimeScroller ()
 
-@property (nonatomic, strong) UIToolbar *toolbar;
-@property (nonatomic, strong) UIView *hours, *minutes;
 @property (nonatomic, strong) NSDate *lastDate;
-@property (nonatomic, strong) UILabel *dateLabel;
+@property (nonatomic, strong) UIView *hours, *minutes;
+@property (nonatomic, strong) UILabel *dateLabel, *timeLabel;
 
 @property (nonatomic, weak) UITableView *tableView;
 @property (nonatomic, weak) UIImageView *scrollBar;
@@ -22,6 +21,8 @@
 @property (nonatomic, copy) NSDateFormatter *dateFormatter;
 
 - (void)captureTableViewAndScrollBar;
+- (void)constructSubviews;
+- (void)createDateFormatters;
 
 @end
 
@@ -31,76 +32,30 @@
 {
     self = [super initWithFrame:frame];
     if (self) {
-        _toolbar = [[UIToolbar alloc] initWithFrame:self.bounds];
-        [self.layer insertSublayer:self.toolbar.layer atIndex:0];
-        
         // If we don't clip to bounds the toolbar draws a thin shadow on top
         self.clipsToBounds = YES;
+        self.layer.contentsScale = [UIScreen mainScreen].scale;
         
         // Create mask layer
         CALayer* maskLayer = [CALayer layer];
-        maskLayer.frame = CGRectMake(0, 0, CGRectGetWidth(frame) , CGRectGetHeight(frame));
-        maskLayer.contents = (__bridge id)[[UIImage imageNamed:@"mask"] CGImage];
+        maskLayer.contents = (__bridge id)[[[UIImage imageNamed:@"mask"] resizableImageWithCapInsets:UIEdgeInsetsMake(0.0f, 30.0f, 0.0f, 20.0f)] CGImage];
+        maskLayer.contentsScale = [UIScreen mainScreen].scale;
+        maskLayer.contentsCenter = CGRectMake(20.0f/100.0f, 0.0f, 60.0f/100.0f, 0.0f);
         
         // Apply the mask layer
-        self.toolbar.layer.mask = maskLayer;
-        self.toolbar.layer.masksToBounds = YES;
-        self.toolbar.backgroundColor = [UIColor colorWithWhite:1.0 alpha:0.8];
-        self.toolbar.barTintColor = [[[[UIApplication sharedApplication] delegate] window] tintColor];
+        self.backgroundColor = [UIColor colorWithWhite:1.0 alpha:0.98];
+        self.layer.mask = maskLayer;
         
-        // Get tint color
-        UIColor *tintColor = [[[[UIApplication sharedApplication] delegate] window] tintColor];
-        if(!tintColor){
-            tintColor = [UIColor colorWithRed:0.0 green:122.0/255.0 blue:1.0 alpha:1.0];
-        }
-        
-        // Clock
-        _hours = [[UIView alloc] initWithFrame:CGRectMake(15.0f, 9.0f, 2.0f, 11.0f)];
-        _hours.layer.anchorPoint = CGPointMake(0.5f, 1.0/11.0f);
-        _hours.layer.cornerRadius = 1.0;
-        _hours.layer.shouldRasterize = YES;
-        _hours.layer.contentsScale = [[UIScreen mainScreen] scale];
-        _hours.backgroundColor = tintColor;
-        [self addSubview:_hours];
-        
-        _minutes = [[UIView alloc] initWithFrame:CGRectMake(15.0f, 8.0f, 2.0f, 13.0f)];
-        _minutes.layer.anchorPoint = CGPointMake(0.5f, 1.0/13.0f);
-        _minutes.layer.cornerRadius = 1.0;
-        _minutes.layer.shouldRasterize = YES;
-        _minutes.layer.contentsScale = [[UIScreen mainScreen] scale];
-        _minutes.backgroundColor = tintColor;
-        [self addSubview:_minutes];
-        
-        UIView *border = [[UIView alloc] initWithFrame:CGRectMake(2.0f, 1.0f, 28.0f, 28.0f)];
-        border.layer.borderColor = tintColor.CGColor;
-        border.layer.borderWidth = 1.0f;
-        border.layer.cornerRadius = 14.0f;
-        [self addSubview:border];
-        
-        // Label
-        _dateLabel = [[UILabel alloc] initWithFrame:CGRectMake(34.0f, 0.0f, 46.0f, 30.0f)];
-        
-        _dateLabel.numberOfLines = 2;
-        _dateLabel.backgroundColor = [UIColor clearColor];
-        _dateLabel.font = [UIFont systemFontOfSize:11.0f];
-        [self addSubview:_dateLabel];
-        
-        // Create date formatter
-        _timeFormatter = [[NSDateFormatter alloc] init];
-        [_dateFormatter setLocale:[[NSLocale alloc] initWithLocaleIdentifier:[[NSLocale currentLocale] localeIdentifier]]];
-        [_timeFormatter setDateFormat:@"hh:mm"];
-        
-        _dateFormatter = [[NSDateFormatter alloc] init];
-        [_dateFormatter setLocale:[[NSLocale alloc] initWithLocaleIdentifier:[[NSLocale currentLocale] localeIdentifier]]];
-        [_dateFormatter setDateStyle:NSDateFormatterShortStyle];
-
+        // Create stuff
+        [self constructSubviews];
+        [self createDateFormatters];
     }
     return self;
 }
 
 - (id)initWithDelegate:(id <TTTimeScrollerDelegate>)timeScrollerDelegate;
 {
-    self = [self initWithFrame:CGRectMake(0.0f, 0.0f, 100.0f, 30.0f)];
+    self = [self initWithFrame:CGRectMake(0.0f, 0.0f, 0.0f, 30.0f)];
     _timeScrollerDelegate = timeScrollerDelegate;
     return self;
 }
@@ -149,13 +104,30 @@
         CGFloat minuteHourAngle = (dateComponents.minute / 60.0f) * 360.0f;
         CGFloat dateHourAngle = (((dateComponents.hour * 60.0f) + dateComponents.minute) / (24.0f * 60.0f)) * 360.0f;
         
-        [UIView animateWithDuration:0.2f delay:0.0f options:(UIViewAnimationOptionCurveEaseInOut | UIViewAnimationOptionBeginFromCurrentState)  animations:^{
+        NSString *timeLabelText = [_timeFormatter stringFromDate:date];
+        NSString *dateLabelText = [_dateFormatter stringFromDate:date];
+        
+        CGSize timeLabelTextSize = [timeLabelText sizeWithAttributes:@{NSFontAttributeName : _timeLabel.font}];
+        CGSize dateLabelTextSize = [dateLabelText sizeWithAttributes:@{NSFontAttributeName : _dateLabel.font}];
+        
+        [UIView animateWithDuration:0.2f delay:0.0f options:(UIViewAnimationOptionCurveEaseInOut | UIViewAnimationOptionBeginFromCurrentState | UIViewAnimationOptionAllowAnimatedContent)  animations:^{
+            // Animate clock rotation
             _hours.transform = CGAffineTransformMakeRotation(dateHourAngle * (M_PI / 180.0f));
             _minutes.transform = CGAffineTransformMakeRotation(minuteHourAngle * (M_PI / 180.0f));
-        } completion:nil];
-        
-        _dateLabel.text = [NSString stringWithFormat:@"%@\r%@", [_timeFormatter stringFromDate:date], [_dateFormatter stringFromDate:date]];
-        
+            
+            // Fit size to labels
+            CGFloat maxWidth = MAX(timeLabelTextSize.width, dateLabelTextSize.width);
+            _timeLabel.frame = CGRectMake(CGRectGetMinX(_timeLabel.frame), CGRectGetMinY(_timeLabel.frame), maxWidth, CGRectGetHeight(_timeLabel.frame));
+            _dateLabel.frame = CGRectMake(CGRectGetMinX(_dateLabel.frame), CGRectGetMinY(_dateLabel.frame), maxWidth, CGRectGetHeight(_dateLabel.frame));
+            
+            // Fit frame
+            CGFloat frameWidth = maxWidth + 50.f;
+            self.frame = CGRectMake(frameWidth * -1, 0.0f, frameWidth, CGRectGetHeight(self.frame));
+            self.layer.mask.frame =self.bounds;
+        } completion:^(BOOL finished){
+            _timeLabel.text = timeLabelText;
+            _dateLabel.text = dateLabelText;
+        }];
         
         _lastDate = date;
     } else {
@@ -185,6 +157,64 @@
             }
         }
     }
+}
+
+- (void)constructSubviews
+{
+    // Get tint color
+    UIColor *tintColor = [[[[UIApplication sharedApplication] delegate] window] tintColor];
+    if(!tintColor){
+        tintColor = [UIColor colorWithRed:0.0 green:122.0/255.0 blue:1.0 alpha:1.0];
+    }
+    
+    // Clock
+    _hours = [[UIView alloc] initWithFrame:CGRectMake(14.0f, 9.0f, 2.0f, 11.0f)];
+    _hours.layer.anchorPoint = CGPointMake(0.5f, 1.0/11.0f);
+    _hours.layer.cornerRadius = 1.0;
+    _hours.layer.shouldRasterize = YES;
+    _hours.layer.contentsScale = [[UIScreen mainScreen] scale];
+    _hours.backgroundColor = tintColor;
+    [self addSubview:_hours];
+    
+    _minutes = [[UIView alloc] initWithFrame:CGRectMake(14.0f, 8.0f, 2.0f, 13.0f)];
+    _minutes.layer.anchorPoint = CGPointMake(0.5f, 1.0/13.0f);
+    _minutes.layer.cornerRadius = 1.0;
+    _minutes.layer.shouldRasterize = YES;
+    _minutes.layer.contentsScale = [[UIScreen mainScreen] scale];
+    _minutes.backgroundColor = tintColor;
+    [self addSubview:_minutes];
+    
+    UIView *border = [[UIView alloc] initWithFrame:CGRectMake(1.0f, 1.0f, 28.0f, 28.0f)];
+    border.layer.borderColor = [UIColor blackColor].CGColor;
+    border.layer.borderWidth = 1.0f;
+    border.layer.cornerRadius = 14.0f;
+    [self addSubview:border];
+    
+    // Label
+    _timeLabel = [[UILabel alloc] initWithFrame:CGRectMake(32.0f, 0.0f, 0.0f, 14.0f)];
+    _timeLabel.backgroundColor = [UIColor clearColor];
+    _timeLabel.font = [UIFont boldSystemFontOfSize:11.0f];
+    [self addSubview:_timeLabel];
+    
+    _dateLabel = [[UILabel alloc] initWithFrame:CGRectMake(32.0f, 15.0f, 0.0f, 14.0f)];
+    _dateLabel.backgroundColor = [UIColor clearColor];
+    _dateLabel.font = [UIFont systemFontOfSize:11.0f];
+    [self addSubview:_dateLabel];
+    
+}
+
+- (void)createDateFormatters
+{
+    _timeFormatter = [[NSDateFormatter alloc] init];
+    _timeFormatter.locale = [[NSLocale alloc] initWithLocaleIdentifier:[[NSLocale currentLocale] localeIdentifier]];
+    _timeFormatter.timeStyle = NSDateFormatterShortStyle;
+    _timeFormatter.doesRelativeDateFormatting = YES;
+    
+    _dateFormatter = [[NSDateFormatter alloc] init];
+    _dateFormatter.locale = [[NSLocale alloc] initWithLocaleIdentifier:[[NSLocale currentLocale] localeIdentifier]];
+    _dateFormatter.dateStyle = NSDateFormatterShortStyle;
+    _dateFormatter.doesRelativeDateFormatting = YES;
+
 }
 
 @end
